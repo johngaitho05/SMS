beab482aaf214e317cd0654565e7019b486480a8
John Gaitho <johngaitho05@gmail.com>
2020-04-17 19:24:09 +0300

    Edit

--- sms.py
+++ sms.py
@@ -1,4 +1,5 @@
 import africastalking
+
 # Authentication variables
 username="app-name"
 api_key="your api-key"
@@ -8,7 +9,7 @@ africastalking.initialize(username,api_key)
 sms= africastalking.SMS
 
 # send an SMS
-response= sms.send("Your message", phone-number)
+response= sms.send("Your message", ['phone-number'])
 
 # Logging into the console
 print(response)
