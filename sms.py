9c8602642ee7f94a5a64a63a621d2e1af0209385
John Gaitho <johngaitho05@gmail.com>
2020-04-17 14:14:29 +0300

    initial commit

---
+++ sms.py
@@ -0,0 +1,14 @@
+import africastalking
+# Authentication variables
+username="app-name"
+api_key="your api-key"
+# Creating an instance of africastalking
+
+africastalking.initialize(username,api_key)
+sms= africastalking.SMS
+
+# send an SMS
+response= sms.send("Your message", phone-number)
+
+# Logging into the console
+print(response)
